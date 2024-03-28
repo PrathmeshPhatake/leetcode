a2743dc95c8078ed18dc8b50304f9ae25dbb0437
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2024-03-28 23:18:05 +0530

    Time: 3 ms (92.07%), Space: 11.6 MB (97.54%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -7,6 +7,7 @@ public:
         for(int bitIndex=0;bitIndex<32;bitIndex++)
         {
             int cnt=0;
+            //this for check bitIndex of num element 
             for(int num=0;num<n;num++)
             {
                 if(nums[num]&(1<<bitIndex))
@@ -14,6 +15,7 @@ public:
                     cnt++;
                 }
             }
+            //if bitindex is divisibale by 3 then this is   this bit ispresent in ans 
             if(cnt%3==1)
             {
                 ans=ans|(1<<bitIndex);
