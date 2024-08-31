e670a550d476a33725e214d126384c31b9530efd
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2024-08-31 23:23:34 +0530

    Time: 131 ms (38.71%) | Memory: 70.6 MB (31.06%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
@@ -1,40 +1,36 @@
 class Solution {
 public:
-    double maxProbability(int n, vector<vector<int>>& edges,
-                          vector<double>& succProb, int start_node,
-                          int end_node) {
-        vector<vector<pair<int, double>>> adjlist(n);
-        
-
-        for (int i = 0; i < succProb.size(); i++) {
-            adjlist[edges[i][0]].push_back({edges[i][1], succProb[i]});
-            adjlist[edges[i][1]].push_back({edges[i][0], succProb[i]});
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
+        // adjslit
+        vector<vector<pair<int ,double>>>adjlist(n);
+        for(int i=0;i<succProb.size();i++)
+        {
+            adjlist[edges[i][0]].push_back({edges[i][1],succProb[i]});
+             adjlist[edges[i][1]].push_back({edges[i][0],succProb[i]});
         }
-        //    {dist,node}
-        vector<double> dist(n, 0);
-        priority_queue<pair<double, int>>pq;
-
-        pq.push({1.0, start_node});
-
-        dist[start_node] = 1.0;
+        // dist and node with max heap beacse of we  wan max size
+        priority_queue<pair<double,int>>pq;
+        vector<double>dist(n,0);
         vector<int>visit(n,0);
-        
-        while (!pq.empty()) {
-            int node = pq.top().second;
-            double distance = pq.top().first;
+        pq.push({1.0,start_node});
+        dist[start_node]=1.0;
+        while(!pq.empty())
+        {
+            int node=pq.top().second;
+            double distance=pq.top().first;
             pq.pop();
             if(visit[node]==1) continue;
             visit[node]=1;
+            for(auto it:adjlist[node])
+            {
+                 int newNode=it.first;
+                 double newdist=it.second;
+                 if(distance*newdist>dist[newNode])
+                 {
+                    dist[newNode]=distance*newdist;
+                    pq.push({dist[newNode],newNode});
 
-
-            for (auto it : adjlist[node]) {
-                int newnode = it.first;
-                double newDist = it.second;
-                if (distance * newDist > dist[newnode] ) {
-                    dist[newnode] = distance * newDist;
-                   
-                    pq.push({dist[newnode], newnode});
-                }
+                 }
             }
         }
         return dist[end_node];
