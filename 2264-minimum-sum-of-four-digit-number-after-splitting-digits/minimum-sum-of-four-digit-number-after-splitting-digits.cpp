92ba8828777a63240666d550ae614dc9ec19591e
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2024-09-04 10:46:29 +0530

    Time: 2 ms (48.59%) | Memory: 7.4 MB (73.91%) - LeetSync

--- 2264-minimum-sum-of-four-digit-number-after-splitting-digits/minimum-sum-of-four-digit-number-after-splitting-digits.cpp
+++ 2264-minimum-sum-of-four-digit-number-after-splitting-digits/minimum-sum-of-four-digit-number-after-splitting-digits.cpp
@@ -4,8 +4,8 @@ public:
         string s=to_string(num);
         sort(s.begin(),s.end());
         int i=0;
-        int num1=(s[i]-'0')*10+s[3]-'0';
-        int num2=(s[i+1]-'0')*10+s[2]-'0';
+        int num1=(s[i]-'0')*10+s[2]-'0';
+        int num2=(s[i+1]-'0')*10+s[3]-'0';
         return num1+num2;
     }
 };
