fb61214d4e468830f3983a5777b553247c21cf1d
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2024-09-14 12:57:42 +0530

    Time: 12 ms (23.87%) | Memory: 15 MB (9.84%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,40 +1,24 @@
 class Solution {
 public:
-int counting(vector<vector<int>>&dp,vector<vector<int>>&grid,vector<vector<int>>&visited,int row,int col)
-{
-    int m=grid.size();
-    int n=grid[0].size();
-    if(row==m-1 && col==n-1)
-    {
-        return  grid[row][col];
-     
-    }
-    if(dp[row][col]!=-1) return dp[row][col];
-    int drow[]={0,1};
-    int dcol[]={1,0};
-    int temp=INT_MAX;
-    for(int i=0;i<2;i++)
-    {
-        int nrow=row+drow[i];
-        int ncol=col+dcol[i];
-        if(nrow<m && ncol<n && visited[nrow][ncol]!=1)
-        {
-            visited[nrow][ncol]=1;
-            temp=min(temp,counting(dp,grid,visited,nrow,ncol));
-            visited[nrow][ncol]=0;
-        }
-    }
-    return dp[row][col]=temp+grid[row][col];
-    
-}
     int minPathSum(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        int tpath=INT_MAX;
-        vector<vector<int>>visited(m,vector<int>(n,0));
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        visited[0][0]=1;
-         return counting(dp,grid,visited,0,0);
-        
+      int m=grid.size();
+      int n=grid[0].size();
+      vector<vector<int>>dp(m,vector<int>(n,INT_MAX));
+      dp[0][0]=grid[0][0];
+      for(int i=0;i<m;i++)
+      {
+        for(int j=0;j<n;j++)
+        {
+            if(i-1>=0)
+            {
+                dp[i][j]=min(dp[i][j],grid[i][j]+dp[i-1][j]);
+            }
+            if(j-1>=0)
+            {
+                dp[i][j]=min(dp[i][j],grid[i][j]+dp[i][j-1]);
+            }
+        }
+      }
+      return dp[m-1][n-1];
     }
 };
