995ea951620bd121218d4ea12142451955fa5fac
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2024-09-15 23:23:12 +0530

    Time: 83 ms (84.84%) | Memory: 98.7 MB (51.67%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,35 +1,36 @@
 class Solution {
-    int  partion(vector<int>&nums,vector<vector<int>>&dp,int index,int target)
-    {
-        if(target==0) return 1;
-        if(index==0) return nums[0]==target ? 1:0;
-        // if(index<0)return 0;
-        
-        if(dp[index][target]!=-1)return dp[index][target];
-
-        int  not_taken=partion(nums,dp,index-1,target);
-        int taken=0;
-
-        if(nums[index]<=target)
-        {
-            taken=partion(nums,dp,index-1,target-nums[index]);
-        }
-        return dp[index][target]=taken || not_taken;
-    }
 public:
     bool canPartition(vector<int>& nums) {
-        int total=0;
         int n=nums.size();
-        for(auto it :nums)
+        int total=0;
+        for(auto it:nums)
         {
             total+=it;
         }
-        // if total is odd then   sit not possible 
-        if(total%2!=0)return false;
-
-        int target=total/2;
-        vector<vector<int>>dp(n,vector<int>(target+1,-1));
-        return partion(nums,dp,n-1,total/2)==1 ;
-
+        if(total%2!=0) return false;
+        int tar=total/2;
+        vector<vector<int>>dp(n,vector<int>(tar+1,0));
+        // base case 
+        // target is 0 it return true
+        for(int i=0;i<n;i++)
+        {
+            dp[i][0]=1;
+        }
+        if(nums[0]<=tar) dp[0][nums[0]]=1;
+        // bootm up approches 
+        for(int index=1;index<n;index++)
+        {
+            for(int target=1;target<=tar;target++)
+            {
+                int non_taken=dp[index-1][target];
+                int taken =0;
+                if(nums[index]<=target)
+                {
+                    taken =dp[index-1][target-nums[index]];
+                }
+               dp[index][target]=taken || non_taken;
+            }
+        }
+        return dp[n-1][tar]==1;
     }
 };
