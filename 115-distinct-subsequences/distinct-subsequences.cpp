5f906135725df0baa20a6c3d53d98231febccd1c
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2024-10-02 00:44:37 +0530

    Time: 27 ms (29.68%) | Memory: 25.7 MB (49.44%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,35 +1,29 @@
 class Solution {
-    int   counting(vector<vector<int>>&dp,int i,int j,string& s,string& t)
-    {
-        if(i<0 || j<0 )
-        {
-            if(j<0)
-            {
-                return 1;
-            }
-            return 0;
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
-        int left=0;
-        int right=0;
-        if(s[i]==t[j])
-        {
-            left=counting(dp,i-1,j-1,s,t);
-            right=counting(dp,i-1,j,s,t);
-        }
-        else
-        {
-            left=counting(dp,i-1,j,s,t);
-        }
-        return dp[i][j]=left+right;
-    }
 public:
     int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
         
-        int n=s.size();
-        int m=t.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return counting(dp,n-1,m-1,s,t);
+       
+        const int MOD = 1e9 + 7;
         
+        vector<vector<int >> dp(n + 1, vector<int >(m + 1, 0));
+        
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+
+        // Fill the DP table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s[i - 1] == t[j - 1]) {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % MOD;
+                } else {
+                    dp[i][j] = dp[i - 1][j] % MOD;
+                }
+            }
+        }
+
+        return  (dp[n][m] % MOD);
     }
 };
