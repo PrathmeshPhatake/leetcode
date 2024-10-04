cda15d10bd41b05312e6fbf1cadbd09e23bbf2bf
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2024-10-04 23:26:11 +0530

    Time: 41 ms (50.82%) | Memory: 31.9 MB (11.27%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,34 +1,33 @@
+// we compare p with s 
+// take not take
 class Solution {
 public:
-// passed  p and s  P contain * and ? 
-bool checker(string& p, string& s,int i,int j,vector<vector<int>>&dp)
+bool func(vector<vector<int>>&dp,int i,int j,string& s,string& p)
 {
     if(i<0 && j<0) return 1;
-    if(i<0 && j>=0) return 0;
-    if(j<0)
+    if(i>=0 && j<0) return 0;
+    if(i<0)
     {
-        while(i>=0)
+        while(j>=0)
         {
-            if(p[i]!='*') return 0;
-            i--;
+            if(p[j]!='*') return 0;
+            j--;
         }
         return 1;
-    }
+    } 
     if(dp[i][j]!=-1) return dp[i][j];
-    if(p[i]==s[j] || p[i]=='?') {
-        return dp[i][j]=checker(p,s,i-1,j-1,dp);
-    }
-    if(p[i]=='*')
-    {
-        return dp[i][j]=checker(p,s,i-1,j,dp) ||  checker(p,s,i,j-1,dp);
+    // if ? comes  or both are same 
+    if(p[j]=='?' || p[j]==s[i]) return dp[i][j]= func(dp,i-1,j-1,s,p);
+    // IF STAR COME S THEN SKIP THIS STAR OR GET LETTER OR SKIP THIS 
+    if(p[j]=='*') {
+        return dp[i][j]=func(dp,i-1,j,s,p) || func(dp,i,j-1,s,p);
     }
     return dp[i][j]=0;
 }
     bool isMatch(string s, string p) {
         int n=s.size();
         int m=p.size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        // passed m-1,n-1
-        return checker(p,s,m-1,n-1,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return func(dp,n-1,m-1,s,p);
     }
 };
