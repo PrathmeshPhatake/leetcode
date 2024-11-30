13e61f667ce711c6bb40d430d4fb819c623ac7ed
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2024-12-01 00:54:56 +0530

    Time: 27 ms (60.80%) | Memory: 27.6 MB (5.31%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,26 +1,47 @@
 class Solution {
 public:
+int func(vector<vector<int>>dp,string text1, string text2,int i,int j)
+{
+    int m=text1.size();
+    int n=text2.size();
+    if(i==m || j==n)
+    {
+        return 0;
+
+    }
+    if(dp[i][j]!=-1)return dp[i][j];
+    // if(i==m || j==n)return;
+    if(text1[i]==text2[j]){
+        dp[i][j]=1+func(dp,text1,text2,i+1,j+1);
+    }
+    else
+    {
+        dp[i][j]=max(func(dp,text1,text2,i,j+1),func(dp,text1,text2,i+1,j));
+    }
+    return dp[i][j];
+    
+}
     int longestCommonSubsequence(string text1, string text2) {
-        int n=text1.size();
-        int m=text2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-       // base case
-       for(int i=0;i<n;i++) dp[i][0]=0;
-       for(int i=0;i<m;i++) dp[0][i]=0;
-        for(int id1=1;id1<=n;id1++)
+        int m=text1.size();
+        int n=text2.size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+        // return func(dp,text1,text2,0,0);
+        for(int i=1;i<=m;i++)
         {
-            for(int id2=1;id2<=m;id2++)
+            for(int j=1;j<=n;j++)
             {
-                if(text1[id1-1]==text2[id2-1])
+                if(text1[i-1]==text2[j-1])
                 {
-                    dp[id1][id2]=1+dp[id1-1][id2-1];
+                    dp[i][j]=1+dp[i-1][j-1];
+
                 }
                 else
                 {
-                    dp[id1][id2]=max(dp[id1][id2-1],dp[id1-1][id2]);
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
-        return dp[n][m];
+        return dp[m][n];
+       
     }
 };
