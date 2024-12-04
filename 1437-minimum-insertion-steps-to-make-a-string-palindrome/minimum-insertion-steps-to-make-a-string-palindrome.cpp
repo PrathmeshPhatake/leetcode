20b1c16b7f90e456b27a843944a55c5573179f1b
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2024-12-04 18:52:06 +0530

    Time: 37 ms (39.02%) | Memory: 31.4 MB (5.38%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-int substring(string s,string t)
+int func(string s,string t)
 {
     int n=s.size();
-    vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-    // base case all each element is palindromic
+    int m=t.size();
+    vector<vector<int>>dp(n+1,vector<int>(m+1,0));
     for(int i=1;i<=n;i++)
     {
-        for(int j=1;j<=n;j++)
+        for(int j=1;j<=m;j++)
         {
             if(s[i-1]==t[j-1])
             {
@@ -15,18 +15,16 @@ int substring(string s,string t)
             }
             else
             {
-                dp[i][j]=max(dp[i][j-1], dp[i-1][j]);
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
             }
         }
+        
     }
-    return dp[n][n];
-
+    return dp[n][m];
 }
     int minInsertions(string s) {
-        int n=s.size();
         string t=s;
         reverse(t.begin(),t.end());
-        int size=substring(s,t);
-        return n-size;
+        return s.size()-func(s,t);
     }
 };
