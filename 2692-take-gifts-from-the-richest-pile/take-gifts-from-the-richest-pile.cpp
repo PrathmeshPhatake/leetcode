394125b0187231dcf87ed860a1cb1d7ebc05738b
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2024-12-12 14:29:34 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (62.09%) - LeetSync

--- 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.cpp
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.cpp
@@ -3,23 +3,20 @@ public:
     long long pickGifts(vector<int>& gifts, int k) {
         // max heap
         long long sum=0;
-        priority_queue<int>pq(gifts.begin(),gifts.end());
-        // for(auto it:gifts)
-        // {
-        //     pq.push(it);
-        // }
+        priority_queue<int>pq;
+        for(auto it:gifts)
+        {
+            pq.push(it);
+            sum+=it;
+        }
         while(k--)
         {
             int value=pq.top();
             pq.pop();
+            sum-=(value-sqrt(value));
             pq.push(sqrt(value));
         }
-        while(!pq.empty())
-        {
-            int value=pq.top();
-            pq.pop();
-            sum+=value;
-        }
-        return sum;
+        
+          return sum;
     }
 };
