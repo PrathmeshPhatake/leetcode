345095e5aabc241c24f5fb7edae2c0caa0eaeb1c
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2024-12-21 14:32:55 +0530

    Time: 3 ms (50.59%) | Memory: 84.1 MB (13.26%) - LeetSync

--- 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.cpp
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.cpp
@@ -6,27 +6,42 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     TreeNode* reverseOddLevels(TreeNode* root) {
-        DFS(root->left, root->right, 0);
-        return root;
-    }
+        queue<TreeNode*> q;
+        q.push(root);
+        int level = 0;
+        while (!q.empty()) {
+            int size = q.size();
+            vector<TreeNode*> currentlevel;
+            for (int i = 0; i < size; i++) {
+                // push node of current level;
+                TreeNode* temp = q.front();
+                q.pop();
+                currentlevel.push_back(temp);
 
-private:
-    void DFS(TreeNode* leftChild, TreeNode* rightChild, int level) {
-        if (leftChild == nullptr || rightChild == nullptr) {
-            return;
+                if (temp->left)
+                    q.push(temp->left);
+                if (temp->right)
+                    q.push(temp->right);
+            }
+
+            if (level % 2 == 1) {
+                int left = 0;
+                int right = currentlevel.size() - 1;
+                while (left < right) {
+                    swap(currentlevel[left]->val, currentlevel[right]->val);
+                    left++;
+                    right--;
+                }
+            }
+            level++;
         }
-        if (level % 2 == 0) {
-            int temp = leftChild->val;
-            leftChild->val = rightChild->val;
-            rightChild->val = temp;
-        }
-        DFS(leftChild->left, rightChild->right, level + 1);
-        DFS(leftChild->right, rightChild->left, level + 1);
+        return root;
     }
 };
