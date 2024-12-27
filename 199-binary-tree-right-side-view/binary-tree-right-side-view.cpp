f02c042699205fca35d11284cad404e3702d5387
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2024-12-27 13:55:58 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (5.42%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+void func(TreeNode*root,int v,int l,map<int,int>&mp)
+{
+    if(root==NULL) return;
+    if(mp.find(l)==mp.end()  )
+    {
+        mp[l]={root->val};
+    }
+    func(root->right,v+1,l+1,mp);
+    func(root->left,v-1,l+1,mp);
+}
+    vector<int> rightSideView(TreeNode* root) {
+        map<int,int>mp;
+        vector<int>ans;
+        func(root,0,0,mp);
+        for(auto it:mp)
+        {
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
