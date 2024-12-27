ce83b202ea40dd0c0168d0676abd73de2bac361c
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2024-12-27 13:47:47 +0530

    Time: 4 ms (81.24%) | Memory: 15.3 MB (43.57%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -2,28 +2,20 @@ class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
-        int totalsum = accumulate(nums.begin(), nums.end(), 0);
-        if(totalsum<target  || (totalsum-target)%2!=0) return 0;
+        int totalsum=accumulate(nums.begin(),nums.end(),0);
         int subsetsum=(totalsum-target)/2;
+        if(totalsum<target || (totalsum-target)%2!=0) return 0;
         vector<vector<int>>dp(n+1,vector<int>(subsetsum+1,0));
-        for(int i=0;i<n;i++)
-        {
-            dp[i][0]=1;
-        }
-         for(int i=1;i<=n;i++)
-         {
-            for(int j=0;j<=subsetsum;j++)
-            {
-                // exclude
+        for(int i=0;i<n;i++) dp[i][0]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=subsetsum;j++){
                 dp[i][j]=dp[i-1][j];
-                // enclude
                 if(j>=nums[i-1])
                 {
                     dp[i][j]+=dp[i-1][j-nums[i-1]];
                 }
             }
-         }
-         return dp[n][subsetsum];
-
+        }
+        return dp[n][subsetsum];
     }
 };
