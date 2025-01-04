69f7f99622653ca2ab526072f7a1cca8166d8a41
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-01-04 23:37:01 +0530

    Time: 23 ms (53.82%) | Memory: 24.7 MB (29.04%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -1,28 +1,63 @@
 class Solution {
 public:
-void dfs(vector<int>&visited,int i,vector<vector<int>>& stones)
+int ulti_parent(int node,vector<int>&parent){
+    if(parent[node]==node)
+    {
+        return node;
+    }
+    return parent[node]=ulti_parent(parent[node],parent);
+}
+void union_rank(int u,int v,vector<int>&parent,vector<int>&rank)
 {
-    int n=visited.size();
-    visited[i]=1;
-    for(int j=0;j<n;j++){
-        if(!visited[j] && (stones[i][0]==stones[j][0]||stones[i][1]==stones[j][1]))
-        {
-            dfs(visited,j,stones);
-        }
+    int ul_u=ulti_parent(u,parent);
+    int ul_v=ulti_parent(v,parent);
+    if(ul_u==ul_v)return;
+    if(rank[ul_u]<rank[ul_v])
+    {
+        parent[ul_u]=parent[ul_v];
+    }
+    else if(rank[ul_v]<rank[ul_u])
+    {
+        parent[ul_v]=parent[ul_u];
+    }else
+    {
+        parent[ul_v]=parent[ul_u];
+        rank[ul_u]=parent[ul_v];
     }
 }
     int removeStones(vector<vector<int>>& stones) {
         int n=stones.size();
-        vector<int>visited(n,0);
-        int count=0;
-        for(int i=0;i<n;i++)
+        int maxrow=INT_MIN;
+        int maxcol=INT_MIN;
+        for(auto it:stones)
         {
-            if(!visited[i])
+            maxrow=max(maxrow,it[0]);
+            maxcol=max(maxcol,it[1]);
+        }
+        vector<int>parent(maxrow+maxcol+2);
+        vector<int>rank(maxrow+maxcol+2,0);
+        for(int i=0;i<parent.size();i++)
+        {
+            parent[i]=i;
+        }
+        
+        unordered_map<int, int> stoneNodes;
+        for (auto it : stones) {
+            int nodeRow = it[0];
+            int nodeCol = it[1] + maxrow + 1;
+            union_rank(nodeRow, nodeCol,parent,rank);
+            stoneNodes[nodeRow] = 1;
+            stoneNodes[nodeCol] = 1;
+        }
+        int count=0;
+        for(auto it:stoneNodes)
+        {
+            if(parent[it.first]==it.first)
             {
-                dfs(visited,i,stones);
                 count++;
             }
         }
         return n-count;
+        
     }
 };
