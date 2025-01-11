a1b3abe2ce7c8eafbee85b1601fd06b5913fc519
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-01-11 15:40:59 +0530

    Time: 7 ms (15.61%) | Memory: 19.9 MB (15.53%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
-int counting(vector<vector<int>>&dp,vector<int>prices,int index,int buy)
-{
-    int n=prices.size();
-    int sum=0;
-    if(index>=n)
-    {
-        return 0;
-    }
-    if(buy)
-    {
-        sum=max(-prices[index]+counting(dp,prices,index+1,0),0+counting(dp,prices,index+1,1));
-    }
-    else
-    {
-       sum=max(prices[index]+counting(dp,prices,index+1,1),0+counting(dp,prices,index+1,0));   
-    }
-    return sum;
-}
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        
-        vector<vector<int>>dp(n+1,vector<int>(2,-1));
-       dp[0][0]=0;
-       dp[0][1]=-prices[0];
-       for(int i=1;i<n;i++)
-       {
-          dp[i][0]=max(dp[i-1][0],dp[i-1][1]+prices[i]);
-          dp[i][1]=max(dp[i-1][1],dp[i-1][0]-prices[i]);
-       }
-       return dp[n-1][0];
+        int n = prices.size();
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+
+        // Tabulation from the last day backward
+        for (int i = n - 1; i >= 0; i--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                if (buy) {
+                    dp[i][buy] = max(dp[i + 1][1], -prices[i] + dp[i + 1][0]);
+                } else {
+                    dp[i][buy] = max(dp[i + 1][0], prices[i] + dp[i + 1][1]);
+                }
+            }
+        }
+
+        return dp[0][1];  // Start with the option to buy
     }
 };
