6209635a00b640c67a37ed1171df807974add56b
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-01-12 00:40:34 +0530

    Time: 15 ms (27.27%) | Memory: 15 MB (33.92%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.cpp
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.cpp
@@ -3,24 +3,19 @@ public:
     bool canConstruct(string s, int k) {
         if(s.size()<k)return false;
         map<char,int>mp;
-        int count=0;
         for(auto it:s)
         {
             mp[it]++;
         }
         map<int,int>counting;
-        for(auto it:mp)
-        {
-            counting[it.second]++;
-        }
-        for(auto it:counting)
-        {
-            if(it.first%2!=0)
+        int count=0;
+        for(auto it:mp){
+            if(it.second%2!=0)
             {
-                count+=it.second;
+                count++;
             }
         }
-         if(count>k)return false;
-         return true;
+        if(count>k)return false;
+        return true;
     }
 };
