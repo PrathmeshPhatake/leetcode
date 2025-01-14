aa5d5842c4394a7205703283bee954cdb2dee53f
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-01-14 12:29:23 +0530

    Time: 589 ms (8.72%) | Memory: 296.6 MB (9.67%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -4,19 +4,17 @@ int func(vector<int>&nums,vector<vector<int>>&dp,int i,int prev_i)
 {
     int n=nums.size();
     if(i==n)return 0;
-    if(dp[i][prev_i+1]!=-1) return dp[i][prev_i+1];
+    if(dp[i][prev_i+1]!=-1)return dp[i][prev_i+1];
     int take=0;
-    int non_take=0+func(nums,dp,i+1,prev_i);
-    if( prev_i==-1||nums[i]>nums[prev_i])
+    if(prev_i==-1 || nums[i]>nums[prev_i])
     {
         take=1+func(nums,dp,i+1,i);
     }
-    
+    int non_take=func(nums,dp,i+1,prev_i);
     return dp[i][prev_i+1]=max(take,non_take);
 }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        // we used n+1 bcz of we have we want prev_i start -1 when prev_i =-1 then get overflow
         vector<vector<int>>dp(n,vector<int>(n+1,-1));
         return func(nums,dp,0,-1);
     }
