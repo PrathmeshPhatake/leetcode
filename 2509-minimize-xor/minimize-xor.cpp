eeb6aee2d8178d0b8c103fafd4d1f08a06915242
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-01-15 21:53:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (10.75%) - LeetSync

--- 2509-minimize-xor/minimize-xor.cpp
+++ 2509-minimize-xor/minimize-xor.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     int minimizeXor(int num1, int num2) {
-        int bitCount = __builtin_popcount(num2); // Count set bits in num2
-        int result = 0;
-
-        // Set bits in result to minimize XOR
-        for (int i = 31; i >= 0 && bitCount > 0; --i) {
-            if (num1 & (1 << i)) {
-                result |= (1 << i);
-                bitCount--;
+        int a = __builtin_popcount(num1);
+        int b = __builtin_popcount(num2);
+        int res = num1;
+        for (int i = 0; i < 32; ++i) {
+            if (a > b && (1 << i) & num1) {
+                res ^= 1 << i;
+                --a;
+            }
+            if (a < b && !((1 << i) & num1)) {
+                res ^= 1 << i;
+                ++a;
             }
         }
-
-        // If there are still bits to set, set them in the lowest positions
-        for (int i = 0; i < 32 && bitCount > 0; ++i) {
-            if (!(result & (1 << i))) {
-                result |= (1 << i);
-                bitCount--;
-            }
-        }
-
-        return result;
+        return res;
     }
 };
