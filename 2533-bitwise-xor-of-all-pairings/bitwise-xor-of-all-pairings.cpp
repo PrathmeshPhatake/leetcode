f53f39938e620861556ef65e2a60aa2f98b9d3a9
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-01-16 17:47:13 +0530

    Time: 0 ms (100.00%) | Memory: 64.4 MB (36.18%) - LeetSync

--- 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
@@ -3,14 +3,17 @@ public:
     int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
         int xorNums1 = 0, xorNums2 = 0;
 
+        // XOR all elements in nums1
         for (int num : nums1) {
             xorNums1 ^= num;
         }
 
+        // XOR all elements in nums2
         for (int num : nums2) {
             xorNums2 ^= num;
         }
 
+        // Calculate result based on parity of array sizes
         int result = 0;
         if (nums1.size() % 2 != 0) {
             result ^= xorNums2;
