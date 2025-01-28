c048e850b6ea621c637edce3a05127a0738b205a
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-01-29 00:11:01 +0530

    Time: 20 ms (29.60%) | Memory: 96.4 MB (67.08%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-int dfs(vector<vector<int>>&visited,vector<vector<int>>& grid,int r,int c)
+int dfs(vector<vector<int>>&grid,vector<vector<int>>&visited,int r,int c)
 {
-     visited[r][c]=1;
+    int count=0;
+    visited[r][c]=1;
+    count=grid[r][c];
     int n=grid.size();
     int m=grid[0].size();
-    int count=0;
-    count+=grid[r][c];
     int row[]={0,1,0,-1};
     int col[]={1,0,-1,0};
     for(int i=0;i<4;i++)
     {
-        int  nr=r+row[i];
+        int nr=r+row[i];
         int nc=c+col[i];
         if(nr>=0 && nc>=0 && nr<n && nc<m && visited[nr][nc]!=1 && grid[nr][nc]>0)
         {
-           
-            count+=dfs(visited,grid,nr,nc);            
+            count+=dfs(grid,visited,nr,nc);
         }
     }
     return count;
 }
     int findMaxFish(vector<vector<int>>& grid) {
+        int maxi=INT_MIN;
         int n=grid.size();
         int m=grid[0].size();
-        int maxi=INT_MIN;
         vector<vector<int>>visited(n,vector<int>(m,0));
         for(int i=0;i<n;i++)
         {
@@ -32,11 +31,10 @@ int dfs(vector<vector<int>>&visited,vector<vector<int>>& grid,int r,int c)
             {
                 if(visited[i][j]==0 && grid[i][j]>0)
                 {
-                  maxi=max(maxi,dfs(visited,grid,i,j));   
+                    maxi=max(maxi,dfs(grid,visited,i,j));
                 }
             }
         }
         return maxi==INT_MIN?0:maxi;
-    
     }
 };
