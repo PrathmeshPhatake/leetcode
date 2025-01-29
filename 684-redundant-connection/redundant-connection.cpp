aeb3e23d6879abd35f778e306b379cd66db23530
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-01-29 21:31:41 +0530

    Time: 3 ms (37.03%) | Memory: 13.1 MB (56.09%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -11,43 +11,38 @@ int ulti_parent(int u,vector<int>&parent)
 bool union_rank(int u,int v,vector<int>&parent,vector<int>&rank)
 {
     int ul_u=ulti_parent(u,parent);
-    int ul_v=ulti_parent(v,parent);
-    if(ul_u==ul_v)
-    {
-        return false;
-    }
-    if(rank[ul_u]<rank[ul_v])
-    {
-        parent[ul_u]=ul_v;
-    }
-    else if(rank[ul_u]>rank[ul_v])
-    {
-        parent[ul_v]=ul_u;
-    }
-    else
-    {
-         parent[ul_v]=ul_u;
-         rank[ul_u]++;
-    }
-    return true;
+        int ul_v=ulti_parent(v,parent);
+        if(ul_u==ul_v)return false;
+        if(rank[ul_u]>rank[ul_v])
+        {
+            parent[ul_v]=ul_u;
+        }
+        else if(rank[ul_u]<rank[ul_v])
+        {
+            parent[ul_u]=ul_v;
+        }
+        else
+        {
+            parent[ul_u]=ul_v;
+            rank[ul_v]++;
+        }
+        return true;
 }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n=edges.size();
         vector<int>parent(n+1,0);
         vector<int>rank(n+1,0);
         vector<int>ans;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<=n;i++)
         {
             parent[i]=i;
         }
         for(auto it:edges)
         {
-            int u=it[0];
-            int v=it[1];
-            if(!union_rank(u,v,parent,rank))
+            if(!union_rank(it[0],it[1],parent,rank))
             {
-                ans.push_back(u);
-                ans.push_back(v);
+                ans.push_back(it[0]);
+                ans.push_back(it[1]);
             }
         }
         return ans;
