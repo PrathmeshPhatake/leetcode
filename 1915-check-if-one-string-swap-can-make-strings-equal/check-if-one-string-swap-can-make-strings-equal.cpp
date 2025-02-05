261afa0a9f74ba2be0a2d3ce4f09e9dc600d8651
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-02-06 01:40:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (36.36%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
-        map<char,int>mp;
+        map<int,int>mp;
         int n=s1.size();
         int m=s2.size();
+        if(n!=m) return false;
         int count=0;
-        if(m!=n) return false;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++)
+        {
             mp[s1[i]]++;
             mp[s2[i]]--;
             if(s1[i]!=s2[i])
             {
                 count++;
+
             }
         }
-        for(auto  it:mp)
+        for(auto it:mp)
         {
-            if(it.second>0 ||it.second<0) return false;
+            if(it.second>0 || it.second<0)
+            {
+                return  false;
+            }
         }
-        return count>2?false:true;
-        
+        if(count>2) return false;
+        return true;
+
     }
 };
