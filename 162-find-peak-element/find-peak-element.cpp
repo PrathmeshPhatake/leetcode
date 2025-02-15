e5dd19ead6fc01829181ac17524bd87c6d6b7573
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-02-15 15:13:37 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (66.23%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -10,18 +10,19 @@ public:
         while(l<=r)
         {
             int mid=(l+r)/2;
-            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])
+            if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1])
             {
                 return mid;
             }
             if(nums[mid]>nums[mid-1] && nums[mid]<nums[mid+1])
             {
                 l=mid+1;
-            }else 
+            }
+            else
             {
                 r=mid-1;
             }
-        } 
+        }
         return -1;
     }
 };
