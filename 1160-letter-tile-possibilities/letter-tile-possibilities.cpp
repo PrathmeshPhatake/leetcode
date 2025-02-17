b66e82ff19713f8bca9d79be0b780061fa352c18
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-02-18 00:18:44 +0530

    Time: 163 ms (5.23%) | Memory: 55.3 MB (5.23%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-void func(map<char,int>&mp,unordered_set<string>&st,string s)
+void func(unordered_map<char,int>mp, set<string>&st,string s)
 {
-    for(auto& it:mp)
-    {
+     for(auto&it :mp)
+     {
         if(it.second>0)
         {
             s+=it.first;
@@ -13,18 +13,16 @@ void func(map<char,int>&mp,unordered_set<string>&st,string s)
             it.second++;
             s.pop_back();
         }
-    }
+     }
 }
-
     int numTilePossibilities(string tiles) {
-        unordered_set<string>st;
-        map<char,int>mp;
+        unordered_map<char,int>mp;
         int n=tiles.size();
+        set<string>st;
         for(int i=0;i<n;i++)
         {
             mp[tiles[i]]++;
         }
-        string s="";
         func(mp,st,"");
         return st.size();
     }
