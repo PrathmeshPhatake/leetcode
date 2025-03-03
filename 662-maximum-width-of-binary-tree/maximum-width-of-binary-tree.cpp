71f51ac156fdff4eac572f0d28d603e60d2105bb
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-03-03 09:46:48 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (9.59%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -6,35 +6,30 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        // formula for finf it 2*(parent index)+(l==1)||(r==2)
-        if (root == NULL)
-            return 0;
-        queue<pair<TreeNode*, long long>> q;
-        q.push({root, 0});
-        int maxwidth = 0;
-        while (!q.empty()) {
-            int n = q.size();
-            long long leftmost = q.front().second;
-            long long rightmost = q.back().second;
-            maxwidth = max(maxwidth, (int)(rightmost - leftmost + 1));
-            for (int i = 0; i < n; i++) {
-                auto [node, index] = q.front();
+        queue<pair<TreeNode*,int>>q;
+        q.push({root,0});
+        long long  maxi=-1;
+        while(!q.empty()){
+            int size=q.size();
+            long long  leftmost=q.front().second;
+            long long  rightmost=q.back().second;
+            maxi=max(maxi,rightmost-leftmost+1);
+            for(int i=0;i<size;i++)
+            {
+                auto [node,val]=q.front();
                 q.pop();
-                long long normalizedIndex = index - leftmost;
-                if (node->left)
-                    q.push({node->left, 2 * normalizedIndex + 1});
-                if (node->right)
-                    q.push({node->right, 2 * normalizedIndex + 2});
+                long long newVal = val - leftmost;
+               if(node->left) q.push({node->left,newVal*2+1});
+               if(node->right) q.push({node->right,newVal*2+2});
+
             }
         }
-    return maxwidth;
+        return maxi;
     }
-}
-;
+};
