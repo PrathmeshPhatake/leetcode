07593e900660e0b268e034d4b57ba334f52c5bca
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-03-03 23:25:30 +0530

    Time: 2973 ms (13.97%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,2 +1,5 @@
-select EmployeeUNI.unique_id ,Employees.name from Employees left outer join EmployeeUNI
-on   Employees.id=EmployeeUNI.id;
+select EmployeeUNI.unique_id,Employees.name from Employees,EmployeeUNI where 
+EmployeeUNI.id=Employees.id
+union all
+select null,Employees.name from Employees where 
+Employees.id not in  (select id from EmployeeUNI);
