d55b5646afb56c4bd81e5e832f1f4d2332513708
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-03-12 01:59:57 +0530

    Time: 883 ms (13.94%) | Memory: 11.7 MB (80.40%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
+int func(int i,int ans,int target,vector<int>&nums)
+{
+    if(i<0 && target==ans)
+    {
+        return 1;
+    }
+    if(i<0)return 0;
+
+    int positive=func(i-1,ans+(1*nums[i]),target,nums);
+    int negative=func(i-1,ans+(-1*nums[i]),target,nums);
+    return positive+negative;
+}
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
-        int totalsum=accumulate(nums.begin(),nums.end(),0);
-        int subsetsum=(totalsum-target)/2;
-        if(totalsum<target || (totalsum-target)%2!=0) return 0;
-        vector<vector<int>>dp(n+1,vector<int>(subsetsum+1,0));
-        for(int i=0;i<n;i++) dp[i][0]=1;
-        for(int i=1;i<=n;i++){
-            for(int j=0;j<=subsetsum;j++){
-                dp[i][j]=dp[i-1][j];
-                if(j>=nums[i-1])
-                {
-                    dp[i][j]+=dp[i-1][j-nums[i-1]];
-                }
-            }
-        }
-        return dp[n][subsetsum];
+        return func(n-1,0,target,nums);
     }
 };
