f423465c5eda116467aaa12e059a447ee89f4911
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-03-12 01:48:31 +0530

    Time: 58 ms (16.87%) | Memory: 49.7 MB (22.44%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
-int func(vector<int>&coins,int amount,int i,vector<vector<int>>&dp)
+int func(int i,vector<int>& coins,int amount,vector<vector<int>>&dp)
 {
-    if(amount==0) return 0;
-    if(i<0) return INT_MAX-1;
-    if(dp[i][amount]!=-1)return dp[i][amount];
-    int take=INT_MAX; 
-    if(amount>=coins[i])
+    if(amount==0 )
     {
-        int subres=func(coins,amount-coins[i],i,dp);
-       if(subres!=INT_MAX)
-       {
-          take=1+subres;
-       }
+        return 0;
     }
-    int nontake=0+func(coins,amount,i-1,dp);
-    return dp[i][amount]=min(take,nontake);
+    if(i<0)
+    {
+        return INT_MAX-1;
+    }
+    if(dp[i][amount]!=-1) return dp[i][amount];
+    int take=INT_MAX;
+    if(coins[i]<=amount)
+    {
+        int sub=func(i,coins,amount-coins[i],dp);
+        if(sub!=INT_MAX-1)
+        {
+            take=1+sub;
+        }
+    }
+    int non_take=func(i-1,coins,amount,dp);
+    return dp[i][amount]=min(take,non_take);
 }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int result= func(coins,amount,coins.size()-1,dp);
-        return result==INT_MAX-1?-1:result;
+        int value=func(n-1,coins,amount,dp);
+        return value==INT_MAX-1?-1:value;
     }
 };
