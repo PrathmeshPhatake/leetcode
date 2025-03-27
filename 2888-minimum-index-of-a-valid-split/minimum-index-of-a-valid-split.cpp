ec23b54222e935d2dd8e30f6aecf4897a7ee90be
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-03-28 01:01:17 +0530

    Time: 32 ms (58.20%) | Memory: 98.6 MB (56.56%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
@@ -3,12 +3,12 @@ public:
     int minimumIndex(vector<int>& nums) {
         int n=nums.size();
         unordered_map<int,int>mp;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++)
+        {
             mp[nums[i]]++;
-            
         }
         int count=0;
-        int dom=-1;
+        int dom=0;
         for(auto [ele,c]:mp)
         {
             if(c>n/2)
@@ -17,15 +17,15 @@ public:
                 dom=ele;
             }
         }
-        int leftcount=0;
         int leftsize=0;
-        int rightcount=0;
         int rightsize=0;
+        int leftcount=0;
+        int rightcount=0;
         for(int i=0;i<n-1;i++)
         {
             if(nums[i]==dom) leftcount++;
-            leftsize=i+1;
             rightcount=count-leftcount;
+            leftsize=i+1;
             rightsize=n-leftsize;
             if(leftcount>leftsize/2 && rightcount>rightsize/2)
             {
