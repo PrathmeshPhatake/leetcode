9a265e75dce35c1e1eef1407a95a40031465323c
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-06-24 22:09:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (91.74%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        return pow(x,n);
+
+    double binaryExpo(double x, long n)
+    {
+        if(n==0){
+            return 1;
+
+        }
+        if(n<0){
+            return 1.0/binaryExpo(x,-n);
+        }
+        if(n%2==1){
+            return x*binaryExpo(x*x,(n-1)/2);
+        }
+        else
+        {
+            return binaryExpo(x*x,n/2);
+        }
     }
+    double myPow(double x, int n) {
+         return binaryExpo(x,static_cast<long>(n));
+    }
+
 };
