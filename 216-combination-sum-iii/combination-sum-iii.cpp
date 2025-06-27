5dec99ada220b4beb891834b58c1b6a6cb3bc477
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-06-27 10:05:19 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (31.79%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,28 +1,27 @@
 class Solution {
-    void sub(vector<vector<int>>&ans,vector<int>&temp,int count,int sum,int n,int k,int i)
-    {
-        if(count==k)
-        {
-            if(sum==n)
-            {
-                ans.push_back(temp);
-            }
-            return;
-
-        }
-        // overflow handling;
-        if(i>9)return;
-        temp.push_back(i);
-        sub(ans,temp,count+1,sum+i,n,k,i+1);
-        temp.pop_back();
-        sub(ans,temp,count,sum,n,k,i+1);
-
-    }
 public:
+void func(vector<vector<int>>&ans,vector<int>&res,int i,int count,int sum,int n,int k)
+{
+    if(sum==n && count==k)
+    {
+        ans.push_back(res);
+        return;
+    }
+    if(sum>n || count>k ||i>9)
+    {
+        return ;
+    }
+    res.push_back(i);
+    func(ans,res,i+1,count+1,sum+i,n,k);
+    res.pop_back();
+    func(ans,res,i+1,count,sum,n,k);
+}
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>>ans;
-        vector<int>temp;
-        sub(ans,temp,0,0,n,k,1);
+        vector<int>res;
+        func(ans,res,1,0,0,n,k);
         return ans;
+
+        
     }
 };
