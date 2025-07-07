17f92f305802a55fca83c11eb83d24daec4008bf
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-07-08 00:03:37 +0530

    Time: 139 ms (5.01%) | Memory: 145 MB (5.08%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,56 +1,58 @@
 class Solution {
 public:
-void dfs(vector<vector<char>>&visited,vector<vector<char>>board,int r,int c)
+void dfs(vector<vector<int>>&visited,vector<vector<char>>board,vector<vector<char>>&ans,int r,int c)
 {
-    int n=visited.size();
-    int m=visited[0].size();
-    visited[r][c]='O';
-    int row[]={0,1,0,-1};
-    int col[]={1,0,-1,0};
+    int n=board.size();
+    int m=board[0].size();
+    visited[r][c]=1;
+    ans[r][c]='O';
+    int row[]={-1,0,1,0};
+    int col[]={0,1,0,-1};
     for(int i=0;i<4;i++)
     {
         int nr=r+row[i];
         int nc=c+col[i];
-        if(nr>=0 && nr<n && nc>=0 && nc<m && visited[nr][nc]=='X' && board[nr][nc]=='O')
+        if(nr>=0 & nr<n && nc>=0 && nc<m && !visited[nr][nc] && board[nr][nc]=='O')
         {
-            dfs(visited,board,nr,nc);
+            dfs(visited,board,ans,nr,nc);
         }
     }
 }
     void solve(vector<vector<char>>& board) {
         int n=board.size();
         int m=board[0].size();
-        vector<vector<char>>visited(n,vector<char>(m,'X'));
+        vector<vector<int>>visited(n,vector<int>(m,0));
+        vector<vector<char>>ans(n,vector<char>(m,'X'));
         for(int i=0;i<n;i++)
         {
-            if(board[i][0]=='O' && visited[i][0]=='X')
+            if(board[i][0]=='O')
             {
-                dfs(visited,board,i,0);
+                dfs(visited,board,ans,i,0);
             }
-            if(board[i][m-1]=='O' && visited[i][m-1]=='X')
+             if(board[i][m-1]=='O')
             {
-                dfs(visited,board,i,m-1);
+                dfs(visited,board,ans,i,m-1);
             }
         }
-         for(int i=0;i<m;i++)
+        for(int i=0;i<m;i++)
         {
-            if(board[0][i]=='O' && visited[0][i]=='X')
+            if(board[0][i]=='O')
             {
-                dfs(visited,board,0,i);
+                dfs(visited,board,ans,0,i);
             }
-            if(board[n-1][i]=='O' && visited[n-1][i]=='X')
+             if(board[n-1][i]=='O')
             {
-                dfs(visited,board,n-1,i);
+                dfs(visited,board,ans,n-1,i);
             }
         }
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                 if(visited[i][j]=='X')
-                 {
+                if(ans[i][j]=='X')
+                {
                     board[i][j]='X';
-                 }
+                }
             }
         }
     }
