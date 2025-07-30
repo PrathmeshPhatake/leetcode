c0521c911eaf48bbff2302f09d63781de3e9cab6
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-07-30 15:41:33 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (89.38%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-int helper(vector<int>&nums,int start,int end)
+int func(int start,int end,vector<int>&nums)
 {
-    if(end==start)return nums[start];
-    int prev2=0;
     int prev1=0;
+    int prev2=0;
     for(int i=start;i<=end;i++)
     {
-        int current =max(prev1,nums[i]+prev2);
+        int curr=max(prev1,(nums[i]+prev2));
         prev2=prev1;
-        prev1=current;
+        prev1=curr;
     }
     return prev1;
 }
@@ -18,7 +17,9 @@ int helper(vector<int>&nums,int start,int end)
         if(n==0) return 0;
         if(n==1) return nums[0];
         
-        // igonore last home   or igoner first home
-        return max(helper(nums,0,n-2),helper(nums,1,n-1));
+        int case1=func(0,n-2,nums);
+        int case2=func(1,n-1,nums);
+        return max(case1,case2);
+
     }
 };
