db98e818bdb445806681a3360b5e11ba0904d711
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-08-05 17:18:49 +0530

    Time: 135 ms (51.31%) | Memory: 16.7 MB (75.43%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int sum=0;
         int n=nums.size();
+        int sum=0;
         for(int i=0;i<n;i++)
         {
             sum+=nums[i];
         }
-        if(sum%2!=0) return false;
-        sum=sum/2;
-        vector<vector<bool>>dp(n,vector<bool>(sum+1,false));
-        for(int i=0;i<n;i++)
+        if(sum%2!=0)
         {
-            dp[i][0]=true;
-
+            return false;
         }
-        if(nums[0]<=sum)
-        dp[0][nums[0]]=true;
+        sum=sum/2;
+        vector<bool>prev(sum+1,false);
+        prev[0]=true;
+        if(nums[0]>0 && nums[0]<=sum)
+        {
+        prev[nums[0]]=true;
+        }
         for(int i=1;i<n;i++)
         {
-            for(int target=1;target<=sum;target++)
+            vector<bool>curr(sum+1,false);
+            for(int j=1;j<=sum;j++)
             {
-                bool ntake=dp[i-1][target];
+                bool ntake=prev[j];
                 bool take=false;
-                if(nums[i]<=target)
+                if(nums[i]<=j)
                 {
-                    take=dp[i-1][target-nums[i]];
+                    take=prev[j-nums[i]];
                 }
-                dp[i][target]=take||ntake;
+                curr[j]=take||ntake;
             }
+            prev=curr;
         }
-        return dp[n-1][sum];
+        return prev[sum];
     }
 };
