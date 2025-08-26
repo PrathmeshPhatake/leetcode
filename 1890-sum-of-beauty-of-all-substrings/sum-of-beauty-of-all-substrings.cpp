2392a692ce4921acab7685b58f2276c5442f5617
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-08-26 20:30:36 +0530

    Time: 142 ms (21.40%) | Memory: 26 MB (38.47%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int beautySum(string s) {
-        int n = s.size();
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            unordered_map<char, int> mp;
-            int maxiCount=INT_MIN;
-            int miniCount=INT_MAX;
-            vector<pair<char, int>> vec;
-            for (int j = i; j < n; j++) {
+        int n=s.size();
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+           unordered_map<char,int>mp;
+            int maxi=INT_MIN;
+            int mini=INT_MAX;
+            for(int j=i;j<n;j++)
+            {
                 mp[s[j]]++;
-
-            maxiCount=max(maxiCount,mp[s[j]]);
-            miniCount=INT_MAX;
-           for(auto it:mp)
-           {
-             if(it.second>0)
-             {
-                miniCount=min(miniCount,it.second);
-             }
-           }
-
-            sum+=(maxiCount-miniCount);
+                maxi=max(maxi,mp[s[j]]);
+                mini=INT_MAX;
+                for(auto it:mp)
+                {
+                    if(it.second>0)
+                    {
+                        mini=min(mini,it.second);
+                    }
+                }
+                sum+=(maxi-mini);
             }
         }
         return sum;
