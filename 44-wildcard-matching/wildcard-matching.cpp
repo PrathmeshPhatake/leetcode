ecc01ed4d2edbd63857d272b3735039ebe96f0b8
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-08-30 00:57:57 +0530

    Time: 33 ms (51.34%) | Memory: 14.8 MB (75.18%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,33 +1,70 @@
-// we compare p with s 
-// take not take
 class Solution {
 public:
-bool func(vector<vector<int>>&dp,int i,int j,string& s,string& p)
+bool func(vector<vector<bool>>&dp,string s,string p,int i,int j)
 {
-    if(i<0 && j<0) return 1;
-    if(i>=0 && j<0) return 0;
-    if(i<0)
+    if(i==0 && j==0) return true;
+    if(j==0 ) 
     {
-        while(j>=0)
-        {
-            if(p[j]!='*') return 0;
-            j--;
-        }
-        return 1;
-    } 
-    if(dp[i][j]!=-1) return dp[i][j];
-    // if ? comes  or both are same 
-    if(p[j]=='?' || p[j]==s[i]) return dp[i][j]= func(dp,i-1,j-1,s,p);
-    // IF STAR COME S THEN SKIP THIS STAR OR GET LETTER OR SKIP THIS 
-    if(p[j]=='*') {
-        return dp[i][j]=func(dp,i-1,j,s,p) || func(dp,i,j-1,s,p);
+        if(i>0) return false; 
     }
-    return dp[i][j]=0;
+    if(dp[i][j]!=false) return dp[i][j];
+    if(i==0)
+    {
+        if(j>0)
+        {
+            for(int jj=j-1;jj>=0;jj--)
+            {
+                if(p[jj]!='*')
+                {
+                    return false;
+                }
+            }
+                return true;
+        }
+    }
+    // if(s[i-1]!=p[j-1])return false;
+
+    if(s[i-1]==p[j-1] ||p[j-1]=='?')return dp[i][j]=func(dp,s,p,i-1,j-1);
+    if(p[j-1]=='*') return dp[i][j]=func(dp,s,p,i-1,j) ||func(dp,s,p,i,j-1);
+    return dp[i][j];
+    
 }
     bool isMatch(string s, string p) {
         int n=s.size();
         int m=p.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return func(dp,n-1,m-1,s,p);
+        vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
+        // return func(dp,s,p,s.size(),p.size());
+        // for(int i=1;i<=n;i++)
+        // {
+        //     dp[i][0]=false;
+        // }
+        // for(int j=1;j<=m;j++)
+        // {
+        //     if(p[j-1]!='*')
+        //     {
+        //         dp[0][j]=false;
+        //     }else
+        //     {
+        //         dp[0][j]=true;
+        //     }
+        // }
+        dp[0][0]=true;
+        for(int j=1;j<=m;j++){
+            if(p[j-1]=='*')
+            dp[0][j]=dp[0][j-1];
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++)
+            {
+                if(s[i-1]==p[j-1] ||p[j-1]=='?')
+                {
+                    dp[i][j]=dp[i-1][j-1];
+                }else if(p[j-1]=='*')
+                {
+                    dp[i][j]=dp[i-1][j]||dp[i][j-1];
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
