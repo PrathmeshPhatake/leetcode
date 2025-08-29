a276902455051e72fbb724db948b130f338fb3bb
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-08-30 01:05:52 +0530

    Time: 31 ms (65.25%) | Memory: 15.3 MB (54.75%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -48,23 +48,32 @@ bool func(vector<vector<bool>>&dp,string s,string p,int i,int j)
         //         dp[0][j]=true;
         //     }
         // }
-        dp[0][0]=true;
+        // dp[0][0]=true;
+        // for(int j=1;j<=m;j++){
+        //     if(p[j-1]=='*')
+        //     dp[0][j]=dp[0][j-1];
+        // }
+        vector<bool>prev(m+1,false);
+        vector<bool>curr(m+1,false);
+        prev[0]=true;
         for(int j=1;j<=m;j++){
             if(p[j-1]=='*')
-            dp[0][j]=dp[0][j-1];
+            prev[j]=prev[j-1];
         }
         for(int i=1;i<=n;i++){
+            fill(curr.begin(), curr.end(), false);
             for(int j=1;j<=m;j++)
             {
                 if(s[i-1]==p[j-1] ||p[j-1]=='?')
                 {
-                    dp[i][j]=dp[i-1][j-1];
+                    curr[j]=prev[j-1];
                 }else if(p[j-1]=='*')
                 {
-                    dp[i][j]=dp[i-1][j]||dp[i][j-1];
+                    curr[j]=prev[j]||curr[j-1];
                 }
             }
+            prev=curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
