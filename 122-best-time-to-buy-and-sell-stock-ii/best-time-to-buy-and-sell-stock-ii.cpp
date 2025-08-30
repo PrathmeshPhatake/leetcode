9470b60c67c97542e16d2c1ed4f1e71b17f03a16
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-08-30 18:57:10 +0530

    Time: 7 ms (21.38%) | Memory: 22.6 MB (5.34%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
+int func( vector<vector<int>>&dp,vector<int>&prices,int i,int profit,int buy)
+{
+    int n=prices.size();
+    if(i==n)
+    {
+        return 0;
+
+    }
+    if(dp[i][buy]!=-1) return dp[i][buy];
+    if(buy)
+    {
+        profit=max(-prices[i]+func(dp,prices,i+1,profit,0),0+func(dp,prices,i+1,profit,1));
+
+    }else
+    {
+                profit=max(prices[i]+func(dp,prices,i+1,profit,1),0+func(dp,prices,i+1,profit,0));
+    }
+    return dp[i][buy]=profit;
+
+}
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+        int profit=INT_MIN;
+        int n=prices.size();
+        vector<vector<int>>dp(n,vector<int>(2,-1));
+        return func(dp,prices,0,profit,1);
 
-        // Tabulation from the last day backward
-        for (int i = n - 1; i >= 0; i--) {
-            for (int buy = 0; buy <= 1; buy++) {
-                if (buy) {
-                    dp[i][buy] = max(dp[i + 1][1], -prices[i] + dp[i + 1][0]);
-                } else {
-                    dp[i][buy] = max(dp[i + 1][0], prices[i] + dp[i + 1][1]);
-                }
-            }
-        }
-
-        return dp[0][1];  // Start with the option to buy
     }
 };
