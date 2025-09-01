bc86cd93599740472d9a60a756059ccc152eadc8
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-09-02 01:40:11 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (33.14%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,8 +1,29 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-       
-        next_permutation(nums.begin(),nums.end());
-        
+
+        // next_permutation(nums.begin(),nums.end());
+        // find piovt element
+        // find exact greater element of before of pivot and replace it
+        // reverse reaming part of from pivot to end of array
+        int ind = -1;
+        int n = nums.size();
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                ind = i;
+                break;
+            }
+        }
+        if (ind == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        for (int i = n - 1; i > ind; i--) {
+            if (nums[i] > nums[ind]) {
+                swap(nums[i], nums[ind]);
+                break;
+            }
+        }
+        reverse(nums.begin() + ind+1, nums.end());
     }
 };
