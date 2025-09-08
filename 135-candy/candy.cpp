efa649db123691701c56419b9e7c280e77798086
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-09-09 02:26:12 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (44.15%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,18 +1,44 @@
 class Solution {
 public:
-    int candy(vector<int>& ratings, int cnt = 0) {
-        int n = ratings.size();
-        vector<int> candies(n, 1);
-
-        for (int i = 1; i < n; i++) 
-            if (ratings[i] > ratings[i - 1])
-                candies[i] = candies[i - 1] + 1;
-        
-        for (int i = n - 1; i > 0; i--) {
-            if (ratings[i - 1] > ratings[i])
-                candies[i - 1] = max(candies[i] + 1, candies[i - 1]);
-            cnt += candies[i - 1];
+    int candy(vector<int>& ratings) {
+        int count=0;
+        int n=ratings.size();
+        vector<int>candy(n,0);
+        for(int i=0;i<n;i++)
+        {
+            if(i==0)
+            {
+                candy[i]=1;
+            }else
+            {
+                if(ratings[i]>ratings[i-1])
+                {
+                    candy[i]=candy[i-1]+1;
+                }else
+                {
+                    candy[i]=1;
+                }
+            }
         }
-        return cnt + candies[n - 1];
+        int profit=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(i==n-1)
+            {
+                profit+=max(1,candy[i]);
+            }else
+            {
+                if(ratings[i]>ratings[i+1])
+                {
+                    candy[i]=max(candy[i],candy[i+1]+1);
+                    profit+=candy[i];
+                }else
+                {
+                    candy[i]=max(candy[i],1);
+                    profit+=candy[i];
+                }
+            }
+        }
+        return profit;
     }
 };
