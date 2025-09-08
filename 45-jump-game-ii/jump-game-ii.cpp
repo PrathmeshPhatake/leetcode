79d3dfe9962838f946de7102974ca06a785be154
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-09-08 22:08:42 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (63.10%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int l=0;
+        int r=0;
+        int count=0;
+        int n=nums.size();
+        while(r<n-1)
+        {
+            int forhtest=0;
+            for(int i=l;i<=r;i++){
+                forhtest=max(forhtest,i+nums[i]);
+            }
+            l=r+1;
+            r=forhtest;
+            count++;
+        }
+    return count;
+    }
+};
