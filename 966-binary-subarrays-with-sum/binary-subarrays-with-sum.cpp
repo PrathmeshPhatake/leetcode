e87ad7c9f023e0bde94481d9d62281e87dc430c1
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-09-13 19:56:06 +0530

    Time: 1 ms (62.05%) | Memory: 35 MB (34.68%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+int func(vector<int>nums,int goal){
+    if(goal<0) return 0;
+    int sum=0;
+    int count=0;
+    int l=0;
+    int n=nums.size();
+    for(int r=0;r<n;r++){
+        sum+=nums[r];
+        while(sum>goal )
+        {
+            sum-=nums[l];
+            l++;
+        }
+        count+=(r-l+1);
+    }
+    return count;
+}
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return func(nums,goal)-func(nums,goal-1);
+    }
+};
