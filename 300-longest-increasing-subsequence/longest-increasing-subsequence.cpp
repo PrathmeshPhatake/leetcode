ce39ff650d756a48a8d868af4b56095bd39211cf
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-09-30 03:09:20 +0530

    Time: 556 ms (18.45%) | Memory: 296.6 MB (13.33%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,42 +1,22 @@
 class Solution {
 public:
-int func(vector<vector<int>>&dp,vector<int>&nums,int i,int previ)
-{
-    int n=nums.size();
-    if(i==n)
-    {
-        return 0;
-    }
-    int take=0;
-    int ntake=0;
-    // we used prev index element and this is firtsly is -1 so we can achived it 
-    if(dp[i][previ+1]!=-1) return dp[i][previ+1];
-    if(previ==-1 || nums[i]>nums[previ])
-    {
-        take=1+func(dp,nums,i+1,i);
-    }
-    ntake=func(dp,nums,i+1,previ);
-    return dp[i][previ+1]=max(take,ntake);
-}
-    int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        // return func(dp,nums,0,-1);
-        
-        for(int i=n-1;i>=0;i--)
-        {
-            for(int j=i-1;j>=-1;j--)
-            {
-                int take=0;
-                if(j==-1 || nums[i]>nums[j])
-                {
-                    take=1+dp[i+1][i+1];
-                }
-                int ntake=dp[i+1][j+1];
-                dp[i][j+1]=max(take,ntake);
-            }
-        }
-        return dp[0][0];
+    int func(vector<int>& nums, int prev_ind, int ind, int n, vector<vector<int>>& dp) {
+        if (ind == n) return 0;
 
+        if (dp[ind][prev_ind + 1] != -1) return dp[ind][prev_ind + 1];
+
+        int len = func(nums, prev_ind, ind + 1, n, dp);
+
+        if (prev_ind == -1 || nums[ind] > nums[prev_ind]) {
+            len = max(len, 1 + func(nums, ind, ind + 1, n, dp));
+        }
+
+        return dp[ind][prev_ind + 1] = len;
+    }
+
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+        return func(nums, -1, 0, n, dp);
     }
 };
