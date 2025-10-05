039e571292f00273a3d4ae712ff2791d1cb003b0
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-10-05 21:35:03 +0530

    Time: 11 ms (67.26%) | Memory: 35.1 MB (18.94%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,44 +1,40 @@
 class Solution {
 public:
-int func(vector<int>&weights,int cap)
-{
-    int n=weights.size();
-    int loads=0;
-    int days=1;
-    for(int i=0;i<n;i++)
-    {
-        if(loads+weights[i]>cap)
-        {
-            days++;
-            loads=weights[i];
-        }
-        else 
-        {
-            loads+=weights[i];
-        }
-    }
-    return days;
-}
-    int shipWithinDays(vector<int>& weights, int days) {
-        int n=weights.size();
-        int low=*max_element(weights.begin(),weights.end());
-        int high=0;
-        for(int i=0;i<n;i++)
-        {
-            high+=weights[i];
-        }
-        while(low<=high)
-        {
-            int mid=(low+high)/2;
-            int numberdays=func(weights,mid);
-            if(numberdays<=days){
-                high=mid-1;
-            }else
-            {
-                low=mid+1;
+    bool func(vector<int>& weights, int days, int limit) {
+        int count = 0;
+        int sum = 0;
+        for (int i = 0; i < weights.size(); i++) {
+            if (sum + weights[i] <= limit) {
+                sum += weights[i];
+            } else {
+                count++;
+                sum = weights[i];
             }
         }
-        return low;
-        
+        if (sum > 0)
+            count++;
+        if (count <= days)
+            return true;
+        return false;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        int n = weights.size();
+        int low = *max_element(weights.begin(), weights.end());
+        int high = 0;
+        int ans = 0;
+        for (auto it : weights) {
+            high += it;
+        }
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (func(weights, days, mid)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
     }
 };
