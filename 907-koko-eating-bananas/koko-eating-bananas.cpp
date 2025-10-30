064a53369625aaed9b5a9e858e11f715a66890c4
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-10-31 00:02:09 +0530

    Time: 8 ms (58.58%) | Memory: 22.9 MB (76.53%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
-    long long  calculateHours(vector<int>& piles, int mid) {
-        long long  totalHours = 0;
-        for (int it : piles) {
-            totalHours += ceil((double)it / mid);
+long long  func(vector<int>&piles,int speed){
+long long totalHours = 0;
+        for (int bananas : piles) {
+            totalHours += ((long long)bananas + speed - 1) / speed;
         }
         return totalHours;
-    }
-
+}
     int minEatingSpeed(vector<int>& piles, int h) {
-        int l = 1, r = *max_element(piles.begin(), piles.end());
-        int ans = r;  // Max speed is the upper bound
-
-        while (l <= r) {
-            int mid = l + (r - l) / 2;  // ✅ Prevents overflow
-
-            long long total = calculateHours(piles, mid);
-            if (total <= h) {
-                ans = mid;  // Store possible answer
-                r = mid - 1;
-            } else {
-                l = mid + 1;
+        int low=1;
+        int high=*max_element(piles.begin(),piles.end());
+        int ans=high;
+        // for(int i=1;i<=maxi;i++){
+        //     int hours=func(piles,i);
+        //     if(hours<=h) return i;
+            
+        // }
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            long long value=func(piles,mid);
+            if(value<=h){
+                ans=mid;
+                high=mid-1;
+            }else
+            {
+                low=mid+1;
             }
         }
         return ans;
