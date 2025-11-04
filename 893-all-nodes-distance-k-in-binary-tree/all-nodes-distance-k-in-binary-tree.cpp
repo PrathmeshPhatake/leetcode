3b2b7c70b0ace064551fb2e92782f2dbe956fe3e
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-11-04 15:15:26 +0530

    Time: 6 ms (38.34%) | Memory: 15.3 MB (82.45%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -9,47 +9,49 @@
  */
 class Solution {
 public:
-void findparent(TreeNode*root,unordered_map<TreeNode*,TreeNode*>&parent)
+void parent(TreeNode*root,unordered_map<TreeNode*,TreeNode*>&mp)
 {
-    // node parent
     queue<TreeNode*>q;
     q.push(root);
-    while(!q.empty()){
-        TreeNode*node=q.front();
-        q.pop();
-        if(node->left) q.push(node->left); parent[node->left]=node;
-        if(node->right) q.push(node->right); parent[node->right]=node;
-    }
-}
-    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-       // used bfs for 
-       unordered_map<TreeNode*,TreeNode*>parent;
-       findparent(root,parent);
-       unordered_set<TreeNode*>seen;
-       queue<TreeNode*>q;
-       seen.insert(target);
-       q.push(target);
-       int dist=0;
-       while(!q.empty()&&dist<k)
-       {
-           int len=q.size();
-           while(len--){
+    while(!q.empty())
+    {
+        
             TreeNode*node=q.front();
             q.pop();
-            // left ,right , parent
-            if(node->left && seen.insert(node->left).second) q.push(node->left);
-            if(node->right && seen.insert(node->right).second) q.push(node->right);
-            if(parent.count(node) && seen.insert(parent[node]).second) q.push(parent[node]);
-           }
-           dist++;
-       } 
-       vector<int>ans;
-       while(!q.empty())
-       {
-        TreeNode*node=q.front();
-        q.pop();
-        ans.push_back(node->val);
+            if(node->left) q.push(node->left); mp[node->left]=(node);
+            if(node->right) q.push(node->right); mp[node->right]=(node);
+            
+    }
+    
+}
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        unordered_map<TreeNode*,TreeNode*>mp;
+        parent(root,mp);
+        queue<TreeNode*>q;
+        unordered_set<TreeNode*>seen;
+        // node dist
+        q.push(target);
+        seen.insert(target);
+        int dist=0;
+        while(!q.empty() && dist<k){
+            int size=q.size();
+            while(size--){
+                TreeNode*node=q.front();
+                q.pop();
+                if(node->left && seen.insert(node->left).second) q.push(node->left); 
+                if(node->right && seen.insert(node->right).second) q.push(node->right); 
+                if(mp.count(node) && seen.insert(mp[node]).second) q.push(mp[node]);
+            }
+            dist++;
+        }
+        vector<int>ans;
+        while(!q.empty())
+        {
+            ans.push_back(q.front()->val);
+            q.pop();
+        }
+        return ans;
 
-       }return ans;
+
     }
 };
