6d5f3d96a3c129513de6b1234568964ae9e8dbed
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-11-09 00:09:41 +0530

    Time: 55 ms (70.90%) | Memory: 21.1 MB (74.29%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string>st(wordList.begin(),wordList.end());
-        queue<pair<string,int>>q;
-        q.push({beginWord,1});
+    int ladderLength(string beginWord, string endWord,
+                     vector<string>& wordList) {
+        queue<pair<string, int>> q;
+        q.push({beginWord, 1});
+        unordered_set<string> st(wordList.begin(), wordList.end());
         st.erase(beginWord);
-        while(!q.empty())
-        {
-            string word=q.front().first;
-            int count=q.front().second;
-
+        while (!q.empty()) {
+            auto [word, step] = q.front();
             q.pop();
-            if(word==endWord) return count;
-            for(int i=0;i<word.size();i++)
-            {
-                char original=word[i];
-                for(char ch='a';ch<='z';ch++)
-                {
-                    word[i]=ch;
-                    if(st.find(word)!=st.end())
+            if (word == endWord)
+                return step;
+            for (int i = 0; i < word.size(); i++) {
+                char temp = word[i];
+                for (char ch = 'a'; ch <= 'z'; ch++) {
                     {
-                        st.erase(word);
-                        q.push({word,count+1});
-                        
+                        word[i] = ch;
+                        if (st.find(word) != st.end()) {
+                            st.erase(word);
+                            q.push({word, step + 1});
+                        }
                     }
-                    
+                    word[i] = temp;
                 }
-                word[i]=original;
             }
         }
-        return 0;
-    }
-};
+            return 0;
+        }
+    };
