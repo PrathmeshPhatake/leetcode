8f6dc089f5df3a1e5b708a9f2d078406a965748f
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-11-08 21:07:36 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (25.20%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,59 +1,57 @@
 class Solution {
 public:
-void dfs(vector<vector<int>>&visited,vector<vector<char>>board,vector<vector<char>>&ans,int r,int c)
-{
-    int n=board.size();
-    int m=board[0].size();
-    visited[r][c]=1;
-    ans[r][c]='O';
-    int row[]={-1,0,1,0};
-    int col[]={0,1,0,-1};
-    for(int i=0;i<4;i++)
-    {
-        int nr=r+row[i];
-        int nc=c+col[i];
-        if(nr>=0 & nr<n && nc>=0 && nc<m && !visited[nr][nc] && board[nr][nc]=='O')
-        {
-            dfs(visited,board,ans,nr,nc);
-        }
-    }
-}
     void solve(vector<vector<char>>& board) {
         int n=board.size();
         int m=board[0].size();
+        vector<vector<char>>temp(n,vector<char>(m,'X'));
         vector<vector<int>>visited(n,vector<int>(m,0));
-        vector<vector<char>>ans(n,vector<char>(m,'X'));
+        queue<pair<int,int>>q;
         for(int i=0;i<n;i++)
         {
             if(board[i][0]=='O')
             {
-                dfs(visited,board,ans,i,0);
+                q.push({i,0});
+                visited[i][0]=1;
             }
              if(board[i][m-1]=='O')
             {
-                dfs(visited,board,ans,i,m-1);
+                q.push({i,m-1});
+                visited[i][m-1]=1;
+
             }
         }
         for(int i=0;i<m;i++)
         {
             if(board[0][i]=='O')
             {
-                dfs(visited,board,ans,0,i);
+                q.push({0,i});
+                visited[0][i]=1;
+
             }
              if(board[n-1][i]=='O')
             {
-                dfs(visited,board,ans,n-1,i);
+                q.push({n-1,i});
+                visited[n-1][i]=1;
+
             }
         }
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
+        int row[]={-1,0,1,0};
+        int col[]={0,1,0,-1};
+        while(!q.empty()){
+            auto [r,c]=q.front();
+            q.pop();
+            temp[r][c]='O';
+            for(int i=0;i<4;i++)
             {
-                if(ans[i][j]=='X')
+                int nr=r+row[i];
+                int nc=c+col[i];
+                if(nr>=0 && nc>=0 && nr<n && nc<m && board[nr][nc]=='O' && !visited[nr][nc])
                 {
-                    board[i][j]='X';
+                    q.push({nr,nc});
+                    visited[nr][nc]=1;
                 }
             }
         }
+        board=temp;
     }
 };
