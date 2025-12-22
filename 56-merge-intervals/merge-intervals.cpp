2a033115137701ec14294427ed13a8635967c485
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2025-12-22 16:26:20 +0530

    Time: 6 ms (48.34%) | Memory: 25.5 MB (6.69%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end(),[](auto &a,auto &b){
-            return a[0]<b[0];
-        });
-        int ptail=INT_MIN;
-        int phead=INT_MAX;
-        vector<vector<int>>ans;
-        for(auto it:intervals)
-        {
-              if(ptail>=it[0])
-              {
-                 ans.pop_back();
-                 ptail=max(ptail,it[1]);
-                 ans.push_back({phead,ptail});
-              }else
-              {
-                ptail=it[1];
-                phead=it[0];
-                ans.push_back({it[0],it[1]});
-              }
-        }
-        return ans;
+       sort(intervals.begin(),intervals.end(),[](const vector<int>&a, const vector<int>&b){
+        return a[0]<b[0];
+       });
+       vector<vector<int>>ans;
+       int ptail=INT_MIN;
+       int phead=INT_MAX;
+       for(auto it:intervals)
+       {
+          if(ptail>=it[0])
+          {
+            ans.pop_back();
+            ptail=max(ptail,it[1]);
+            ans.push_back({phead,ptail});
+          }else
+          {
+            ptail=it[1];
+            phead=it[0];
+            ans.push_back({it[0],it[1]});
+          }
+       }
+       return ans;
     }
 };
