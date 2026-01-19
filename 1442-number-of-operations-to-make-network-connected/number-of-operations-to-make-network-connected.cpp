48972a2f98ded15dc6dc27865b9b61b9b9fbf05e
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2026-01-19 22:50:53 +0530

    Time: 32 ms (22.19%) | Memory: 48.2 MB (30.67%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,54 +1,50 @@
 class Solution {
 public:
-int unlti_parent(int node,vector<int>&parent)
+int ultimate_parent(vector<int>&parent,int node)
 {
-    if(node==parent[node])return node;
-    return parent[node]=unlti_parent(parent[node],parent);
-}
-void union_rank(int u,int v,vector<int>&parent,vector<int>&rank)
-{
-    int p_u=unlti_parent(u,parent);
-    int p_v=unlti_parent(v,parent);
-    if(p_u==p_v)return;
-    if(rank[p_u]<rank[p_v])
+    if(node==parent[node])
     {
-        parent[p_u]=p_v;
-    }else if(rank[p_u]>rank[p_v])
-    {
-        parent[p_v]=p_u;
-    }else
-    { 
-        parent[p_v]=p_u;
-        rank[p_u]+=rank[p_v];
+        return node;
     }
+    return parent[node]=ultimate_parent(parent,parent[node]);
 }
+
     int makeConnected(int n, vector<vector<int>>& connections) {
-         vector<int>parent(n);
-         vector<int>rank(n,0);
-         if(connections.size()<n-1)  return -1;
-         for(int i=0;i<n;i++)
-         {
+        if(connections.size()<n-1)return -1;
+        vector<int>parent(n,0);
+        vector<int>rank(n,0);
+        for(int i=0;i<n;i++)
+        {
             parent[i]=i;
-         }
-         int count=0;
-         sort(connections.begin(),connections.end());
-         for(auto it:connections)
-         {
-            int u=it[0];
-            int v=it[1];
-             if(unlti_parent(u,parent)!=unlti_parent(v,parent))
-             {
-                union_rank(u,v,parent,rank);
-             }
-             
-         }
-         for(int i=0;i<n;i++)
-         {
+        }
+        for(auto it:connections)
+        {
+            int ulu=ultimate_parent(parent,it[0]);
+            int ulv=ultimate_parent(parent,it[1]);
+            if(ulu==ulv)continue;
+            if(rank[ulu]<rank[ulv])
+            {
+                parent[ulu]=ulv;
+            }else if(rank[ulu]>rank[ulv])
+            {
+                parent[ulv]=ulu;
+            }else
+            {
+                parent[ulu]=ulv;
+                rank[ulv]++;
+            }
+        }
+        int count=0;
+        unordered_set<int>st;
+        for(int i=0;i<n;i++)
+        {
             if(parent[i]==i)
             {
-                count++;
+                // count++;
+                st.insert(i);
             }
-         }
-         return count-1;
+        }
+        return st.size()-1;
+        
     }
 };
