7dc434d2c18ae9a097edd01ca7691c4b360d6384
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2026-01-30 11:55:21 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (47.46%) - LeetSync

--- 1022-unique-paths-iii/unique-paths-iii.cpp
+++ 1022-unique-paths-iii/unique-paths-iii.cpp
@@ -1,41 +1,36 @@
 class Solution {
-private:
-    int solve(vector<vector<int>>& grid, int i, int j, int empty) {
-        // Base case
-        if (i >= grid.size() || i < 0 || j >= grid[0].size() || j < 0 || grid[i][j] == -1) return 0;
-        if (grid[i][j] == 2) return empty == 0 ? 1 : 0;  // Check if all empty cells are visited
-
-        // Mark the cell as visited
-        grid[i][j] = -1;
+public:
+    int func(vector<vector<int>>&grid, int r, int c, int empty) {
+        int n = grid.size();
+        int m = grid[0].size();
+        if (r >= n || c >= m || r < 0 || c < 0 || grid[r][c] == -1)
+            return 0;
+        if (grid[r][c] == 2)
+            return empty == 0 ? 1 : 0;
+        grid[r][c] = -1;
         empty--;
 
-        // Explore the 4 possible directions
-        int paths = solve(grid, i + 1, j, empty) + solve(grid, i - 1, j, empty) + solve(grid, i, j + 1, empty) + solve(grid, i, j - 1, empty);
-
-        // Backtrack and unmark the cell as visited
-        grid[i][j] = 0;
+        int path = func(grid, r-1, c, empty)+func(grid, r, c+1, empty)+func(grid, r+1, c, empty)+func(grid, r, c-1, empty);
+        grid[r][c]=0;
         empty++;
-        
-        return paths;
+        return path;
     }
-
-public:
     int uniquePathsIII(vector<vector<int>>& grid) {
-        int start_x = 0, start_y = 0, empty = 1; // Start with 1 for the starting square itself
-
-        // Find the starting point and count the number of empty cells
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[i].size(); j++) {
+        int start = 0;
+        int end = 0;
+        int empty = 1;
+        int n = grid.size();
+        int m = grid[0].size();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 1) {
-                    start_x = i;
-                    start_y = j;
+                    start = i;
+                    end = j;
                 } else if (grid[i][j] == 0) {
                     empty++;
                 }
             }
         }
-
-        // Start the recursion from the start point
-        return solve(grid, start_x, start_y, empty);
+        return func(grid, start, end, empty);
     }
 };
