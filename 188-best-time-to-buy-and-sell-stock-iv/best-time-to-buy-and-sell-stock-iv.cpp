37305b6f16f2bdda8d8079e930899944a6d0902c
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2026-02-02 14:16:33 +0530

    Time: 8 ms (51.28%) | Memory: 18.1 MB (47.16%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-int func(vector<int> prices,vector<vector<vector<int>>>&dp,int i,int buy,int k,int x)
-{
-    int n=prices.size();
-    if(i>=n || k>=x)
-    {
-        return 0;
-    }
-    if(dp[i][buy][k]!=-1) return dp[i][buy][k];
-    if(buy){
-        dp[i][buy][k]=max(-prices[i]+func(prices,dp,i+1,0,k,x),func(prices,dp,i+1,1,k,x));
-    }else
-    {
-        dp[i][buy][k]=max(prices[i]+func(prices,dp,i+1,1,k+1,x),func(prices,dp,i+1,0,k,x));
-    }
-    return dp[i][buy][k];
-}
     int maxProfit(int k, vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(k,-1)));
-       return  func(prices,dp,0,1,0,k);
-
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(k+1,0)));
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=0;j<=1;j++)
+            {
+                for(int x=1;x<=k;x++)
+                {
+                    // buy
+                    if(j==1)
+                    {
+                        dp[i][j][x]=max(dp[i+1][j][x],-prices[i]+dp[i+1][0][x]);
+                    }else
+                    {
+                        dp[i][j][x]=max(dp[i+1][j][x],prices[i]+dp[i+1][1][x-1]);
+                        
+                    }
+                }
+            }
+        }
+        return dp[0][1][k];
     }
 };
