20c9f2b425a7a29094d71eb6f80eb5dc52d974d0
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2026-02-13 11:53:59 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (82.39%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if (s.empty() || t.empty()) return "";
-
-        vector<int> freq(256, 0);
-        for (char c : t) freq[c]++;
-
-        int left = 0, right = 0, minlen = INT_MAX, start = 0;
+        vector<int>freq(256,0);
+        for(char c:t) freq[c]++;
+        int j=0;
         int n=s.size();
         int m=t.size();
+        int maxlen=INT_MAX;
+        int start=0;
         int cnt=0;
-        int required = t.size();
-        int j=0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++)
+        {
             if(freq[s[i]]>0) cnt++;
             freq[s[i]]--;
-            while(cnt==m)
-            {
-                if(i-j+1<minlen)
+            while(cnt==m){
+                if(i-j+1<maxlen)
                 {
-                    minlen=i-j+1;
+                    maxlen=i-j+1;
                     start=j;
                 }
                 freq[s[j]]++;
-                if(freq[s[j]]>0)cnt--;
+                if(freq[s[j]]>0) cnt--;
                 j++;
             }
-
         }
-  return minlen == INT_MAX ? "" : s.substr(start, minlen);    }
+        return maxlen==INT_MAX?"":s.substr(start,maxlen);
+    }
 };
