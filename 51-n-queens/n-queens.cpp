f6ea015c3319bfc2a97e5707e1af84aa38829dbf
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2026-03-23 02:35:22 +0530

    Time: 4 ms (31.69%) | Memory: 12.5 MB (12.73%) - LeetSync

---
+++ 51-n-queens/n-queens.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool issafe(vector<vector<string>>&ans, vector<vector<char>>& board,int r, int c, int n) {
+        // check all coloumns
+        for (int i = 0; i < c; i++) {
+            if (board[r][i] == 'Q') {
+                return false;
+            }
+        }
+        for (int i = r, j = c; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+        for (int i = r, j = c; i < n && j >= 0; i++, j--) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+        return true;
+    }
+    void func(vector<vector<string>>& ans, vector<vector<char>>& board, int c,
+              int n) {
+        if (c == n) {
+            vector<string> temp;
+            for (int i = 0; i < n; i++) {
+                string row(board[i].begin(), board[i].end());
+                temp.push_back(row);
+            }
+            ans.push_back(temp);
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (issafe(ans,board, i, c, n)) {
+                board[i][c] = 'Q';
+                func(ans, board, c + 1, n);
+                board[i][c] = '.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<vector<char>> board(n,vector<char>(n,'.'));
+        func(ans, board, 0, n);
+        return ans;
+    }
+};
