533b49956f4cac4cb712efd59d3d010a019be9be
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2026-06-03 02:01:13 +0530

    Time: 78 ms (5.04%) | Memory: 152.5 MB (5.09%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -1,36 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
+ListNode*func(ListNode*curr,ListNode*prev)
+{
+    if(curr==NULL)
+    {
+        return prev;
+    }
+    ListNode*temp=new ListNode(curr->val);
+    temp->next=prev;
+    prev=temp;
+    curr=curr->next;
+    return func(curr,prev);
+}
     bool isPalindrome(ListNode* head) {
-        if (!head || !head->next) return true;
-
-        // Step 1: Find middle using slow & fast
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while(fast->next && fast->next->next)
+        ListNode*curr=head;
+        ListNode*prev=NULL;
+        ListNode*revhead=func(curr,prev);
+        while(head!=NULL && revhead!=NULL)
         {
-            slow=slow->next;
-            fast=fast->next->next;
+            if(head->val!=revhead->val)
+            {
+                return false;
+            }
+            head=head->next;
+            revhead=revhead->next;
         }
-
-        // Step 2: Reverse second half
-        ListNode* prev = nullptr;
-        ListNode* curr = slow->next;
-        while (curr) {
-            ListNode* nextNode = curr->next; 
-            curr->next = prev;
-            prev = curr;
-            curr = nextNode;
-        }
-
-        // Step 3: Compare both halves
-        ListNode* p1 = head;
-        ListNode* p2 = prev;
-        while (p2) { 
-            if (p1->val != p2->val) return false;
-            p1 = p1->next;
-            p2 = p2->next;
-        }
-
         return true;
     }
 };
