d3c26dd7202f56b00df6821f56626da3bdad8e82
PrathmeshPhatake <155132958+PrathmeshPhatake@users.noreply.github.com>
2026-06-02 18:57:23 +0530

    Time: 340 ms (13.13%) | Memory: 38.9 MB (27.11%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n=s.length();
-        vector<vector<bool>>dp(n,vector<bool>(n,false));
+        int n=s.size();
         string ans="";
-        for(int len=1;len<=n;len++)
-        {
-            for(int i=0;i+len-1<n;i++){
-                int j=i+len-1;
-                // if len is 1 then 
-                if(len==1) 
+        vector<vector<bool>>dp(n,vector<bool>(n,false));
+        for(int l=1;l<=n;l++){
+            for(int i=0;i+l-1<n;i++)
+            {
+                int j=i+l-1;
+                if(l==1)
                 {
                     dp[i][j]=true;
-                }else if(len==2)
+                }else if(l==2)
                 {
-                    //  if 2 then check it same or not 
                     dp[i][j]=(s[i]==s[j]);
                 }else
                 {
-                    // if first and last is same then we can check middle one is same 
+                    // third element 
                     dp[i][j]=(s[i]==s[j] && dp[i+1][j-1]);
                 }
-                if(dp[i][j] && len>ans.size())
+                if(dp[i][j] && l>ans.size())
                 {
-                    ans=s.substr(i,len);
+                    ans=s.substr(i,l);
                 }
             }
         }
